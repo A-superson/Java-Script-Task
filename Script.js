0caaa9f68bdede9f11956aa37d9f2077f2787cc1
Mojisola Ayoola <Hayosuperson@gmail.com>
2020-03-27 21:25:14 -0700

    change in Script.js

---
+++ Script.js
@@ -0,0 +1,27 @@
+// Variable
+
+const Name = 'Ayoola Samuel';
+    
+console.log (Name);
+
+const UserName = 'Superson';
+console.log (UserName);
+//Array
+
+const courses =['Javascript', 'HTML', 'CSS', 'C#', 'PHP', 'GO', 'KOTLINE'];
+
+console.log(courses);
+
+
+for (var i = 0; i<=200; i++){
+  
+if (i%2===1) {
+console.log(i + 'is an odd number!\n');
+  }
+    
+}
+    
+
+
+
+
